e93a2af44ce30d59af4146973bf608855769d5df
Akhdan Hifzhan <akhdanx@gmail.com>
2023-12-26 18:02:30 +0700

    add postman collection.json

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -8,21 +8,14 @@ class Database
 
     public static function getConnection()
     {
-        // $host = $_ENV['DB_HOST'];
-        // $port = $_ENV['DB_PORT'];
-        // $db   = $_ENV['DB_NAME'];
-        // $user = $_ENV['DB_USERNAME'];
-        // $pass = $_ENV['DB_PASSWORD'];
-
-        // for docker
-        $host = getenv('PGHOST');
-        $db   = getenv('PGDATABASE');
-        $user = getenv('PGUSER');
-        $pass = getenv('PGPASSWORD');
+        $host = $_ENV['DB_HOST'];
+        $port = $_ENV['DB_PORT'];
+        $db   = $_ENV['DB_NAME'];
+        $user = $_ENV['DB_USERNAME'];
+        $pass = $_ENV['DB_PASSWORD'];
 
         if (!self::$conn) {
-            // self::$conn = pg_connect("host=$host port=$port dbname=$db user=$user password=$pass");
-            self::$conn = pg_connect("host=$host dbname=$db user=$user password=$pass");
+            self::$conn = pg_connect("host=$host port=$port dbname=$db user=$user password=$pass");
         }
 
         if (!self::$conn) {
